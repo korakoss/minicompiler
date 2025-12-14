57a020e1eb03eca413b1869b90a02407069232b2
Akos Korosi <akoskorosi@Akos-MacBook-Air.local>
2025-12-14 12:50:04 +0100

    sketched out some structs to put in use for better parsing

--- src/main.rs
+++ src/main.rs
@@ -201,6 +201,58 @@ fn lex(program: &str) -> Vec<Token> {
     tokens
 }
 
+#[derive(Clone)]
+enum Type {
+    Int,
+    Bool,
+    // Pointer,
+    // Struct,
+    // Array
+}
+
+
+#[derive(Clone)]
+struct Variable {
+    name: String,
+    vartype: Type,
+}
+
+
+struct Scope {
+    external_variables: Vec<Variable>,
+    scope_variables: Vec<Variable>,
+    inside_func: bool,
+    inside_loop: bool,
+}
+
+impl Scope {
+
+    fn create_global() -> Scope {               // Phase out for main()
+        Scope {
+            external_variables: Vec::new(),
+            scope_variables: Vec::new(),
+            inside_func: false,
+            inside_loop: false,
+        }
+    }
+
+    fn descend(ancestor_scope: Scope, loop_entry: bool) -> Scope {
+       let mut new_externals = ancestor_scope.external_variables.clone();
+       new_externals.extend(ancestor_scope.scope_variables);
+       let new_inloop = loop_entry || ancestor_scope.inside_loop;
+       Scope {
+            external_variables: new_externals,
+            scope_variables: Vec::new(),
+            inside_func: ancestor_scope.inside_func,
+            inside_loop: new_inloop,
+       }
+    } 
+
+    fn is_defined_variable(&self, name: String) -> bool {
+        self.external_variables.iter().any(|var| var.name == name)|| self.scope_variables.iter().any(|var| var.name == name)
+    }
+}
+
 
 struct Parser {
     tokens: Vec<Token>,
@@ -255,9 +307,6 @@ impl Parser {
     }
     
     fn parse_funccall_args(&mut self) -> Vec<Box<Expression>> {
-        if self.peek() == &Token::RightParen {
-            println!("Boo");
-        }
         let mut args = Vec::new();
         if self.is_expression_start() {
             args.push(Box::new(self.parse_expression()));
@@ -290,7 +339,7 @@ impl Parser {
             },
             _ => {
                 let tok = self.consume();
-                panic!("Unexpected token {:?} during expression parsing at position {:?}", tok, self.position);
+                panic!("Unexpected token {:?} during expression parsing at position {:?}", tok, self.position);   // WRONG -- already consumed
             },
         }
     }
